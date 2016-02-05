094b096c6b3a237575bae558d87a4286fdabdd41
Matt Dwen <matt@group6.co.nz>
2016-02-05 14:47:15 +1300

    Watch for new thumbnails

---
+++ src/mooch.client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using mooch.client.Services;
+
+namespace mooch.client
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var thumbnailWatcher = new ThumbnailWatcher();
+      thumbnailWatcher.LogMessage += ThumbnailWatcher_LogMessage;
+
+      Console.WriteLine("********************");
+      Console.WriteLine("Mooch Client running");
+      Console.WriteLine("********************");
+      Console.ReadKey();
+    }
+
+    private static void ThumbnailWatcher_LogMessage(string message)
+    {
+      Console.WriteLine($"{DateTime.Now} {message}");
+    }
+  }
+}
---
+++ src/mooch.client/Services/ThumbnailWatcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace mooch.client.Services
+{
+  public class ThumbnailWatcher
+  {
+    #region Constructor
+
+    public ThumbnailWatcher()
+    {
+      var path = ConfigurationManager.AppSettings["security.thumbs.path"];
+
+      var watcher = new FileSystemWatcher(path)
+      {
+        Filter = "*_large.jpg"
+      };
+      watcher.Created += _watcher_Created;
+      watcher.EnableRaisingEvents = true;
+    }
+
+    #endregion Constructor
+
+    #region Delegates
+
+    public delegate void LogMessageEventHandler(string message);
+
+    #endregion Delegate
+
+    #region Events
+
+    public event LogMessageEventHandler LogMessage;
+
+    #endregion Events
+
+    #region Event Handlers
+
+    private void _watcher_Created(object sender, FileSystemEventArgs e)
+    {
+      LogMessage?.Invoke("Motion detected");
+    }
+
+    #endregion Event Handlers
+  }
+}
